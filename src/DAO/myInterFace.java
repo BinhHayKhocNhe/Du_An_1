8b7f505d0c13a545d192d508133552f707b7c595
Duong Minh Binh <clonechuaxu1@gmail.com>
2023-11-12 14:34:28 +0700

    Add interFace

---
+++ src/DAO/myInterFace.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author Duong Minh Binh
+ */
+public interface myInterFace<EntityType, KeyType> {
+
+    public void insert(EntityType entity);
+
+    public void update(EntityType entity);
+
+    public void delete(KeyType id);
+
+    EntityType selectById(KeyType id);
+
+    List<EntityType> selectAll();
+
+    List<EntityType> selectBySql(String sql, Object... args);
+}
