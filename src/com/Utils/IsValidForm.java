8d454710fc5a5af0492105d255e376c154e00e86
Duong Minh Binh <clonechuaxu1@gmail.com>
2023-11-25 21:54:15 +0700

    add utils validate form

---
+++ src/com/Utils/IsValidForm.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Utils;
+
+import java.util.regex.Pattern;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Duong Minh Binh
+ */
+public class IsValidForm {
+
+    public static boolean checkNullInformation(JTextField... textFields) {
+        for (JTextField textField : textFields) {
+            if (textField.getText().isEmpty()) {
+                Message.alert(null, "Please enter complete information !");
+                textField.requestFocus();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidEmail(String email) {
+        final Pattern pattern
+                = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+        return pattern.matcher(email).matches();
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("\\d{10}");
+    }
+}
